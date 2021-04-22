1a9555664f876f823f24fc33c171ce8e8f21cddb
Tristan Robert <tristan.robert.44@gmail.com>
2021-04-22 16:05:13 +0200

    fix: :bug: json repositories syntax

--- src/index.js
+++ src/index.js
@@ -7,15 +7,15 @@ async function run() {
   try {
     const repositoriesString = core.getInput("repositories");
     const repositories = JSON.parse(repositoriesString);
+    core.info(`Repositories JSON as ${JSON.stringify(repositories)} ...`);
     const token = core.getInput("token");
     core.setSecret(token);
     const output = core.getInput("output");
-    var repo;
-    var results;
     const allResults = JSON.parse('[]');
-    for (repo in repositories) {
+    repositories.forEach()
+    for (var repo in repositories) {
       core.info(`Fetching dependabot alerts from Github on ${repo} ...`);
-      results = await alerts(repo, token);
+      var results = await alerts(repo, token);
       allResults.push(results.repository);
     }
     fs.writeFileSync(output, JSON.stringify(allResults));
