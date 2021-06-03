55008da45a471f89dda42dc590ad15eef8ee18af
Tristan Robert <tristan.robert.44@gmail.com>
2021-06-03 16:13:39 +0200

    feat: add sum of grades and alerts Fixes #7

--- src/index.js
+++ src/index.js
@@ -3,6 +3,38 @@ const core = require("@actions/core");
 
 const alerts = require("./alerts");
 
+function maxGrade(gradesArray) {
+  const grades = new Map();
+  grades.set("F", 6);
+  grades.set("E", 5);
+  grades.set("D", 4);
+  grades.set("C", 3);
+  grades.set("B", 2);
+  grades.set("A", 1);
+  const orders = new Map();
+  orders.set(6, "F");
+  orders.set(5, "E");
+  orders.set(4, "D");
+  orders.set(3, "C");
+  orders.set(2, "B");
+  orders.set(1, "A");
+  return orders.get(Math.max(...gradesArray.map((grade) => grades.get(grade))));
+}
+
+function sumRepositoriesAlerts(repositories) {
+  var sum = {};
+  sum.totalCount = repositories
+    .filter(Boolean)
+    .map((repository) => repository.vulnerabilityAlerts.totalCount)
+    .reduce((prev, curr) => prev + curr, 0);
+  const grades = repositories
+    .filter(Boolean)
+    .map((repository) => repository.grade);
+  sum.grade = maxGrade(grades);
+  sum.repositories = repositories;
+  return sum;
+}
+
 async function run() {
   try {
     const repositoriesString = core.getInput("repositories");
@@ -13,12 +45,12 @@ async function run() {
     const output = core.getInput("output");
     const maxAlerts = core.getInput("maxAlerts");
     const max = parseInt(maxAlerts);
-    var allResults = [];
+    var repositoriesResults = [];
     await Promise.all(repositories.map(async (repo) => {
       var results = await alerts(repo, token, max);
-      allResults.push(results.repository);
+      repositoriesResults.push(results.repository);
     }));
-    fs.writeFileSync(output, JSON.stringify(allResults));
+    fs.writeFileSync(output, JSON.stringify(sumRepositoriesAlerts(repositoriesResults)));
   } catch (error) {
     core.setFailed(error.message);
   }
